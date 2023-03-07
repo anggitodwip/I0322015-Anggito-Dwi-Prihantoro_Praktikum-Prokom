42a1318e5c5ed281c2a4e0da4bbe762de928ef85
anggitodwip <gitoprihantoro@gmail.com>
2023-03-07 15:37:45 +0700

    SR

--- Anggito.py
+++ Anggito.py
@@ -1,5 +1,5 @@
 print("=  "*10+"=")
 print("="+"P U N Y A".center(29)+"=")
 print("="+"A N G G I T O".center(29)+"=")
-print("="+"SALAM KENAL"+"=")
+print("="+"S A L A M  K E N A L".center(29)+"=")
 print("= ="*10+"=")
