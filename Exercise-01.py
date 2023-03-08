71be5edad2387de4a6f58ee26cf258e7d448ed70
anggitodwip <gitoprihantoro@gmail.com>
2023-03-08 15:01:02 +0700

    Exercise-01

---
+++ Exercise-01.py
@@ -0,0 +1,27 @@
+print("\n"+"Anggito Dwi Prihantoro")
+print("I0322015")
+
+print("="*60)
+
+print("""
+    Perkenalkan nama saya Anggito Dwi Prihantoro, NIM 
+I0322015, mahasiswa Teknik Industri UNS angkatan 2022. Saya 
+berasal dari Kab. Karanganyar dengan alamat Perum. Bejen 
+Asri Rt05/Rw11, Bejen, Karanganyar.
+    Saya asli dan sudah bertempat tinggal di Karanganyar 
+sejak lahir. Tempat tinggal saya, kelurahan Bejen terletak 
+tidak jauh dari jalan utama, namun keadaan di sini begitu 
+tenang karena tidak banyak kendaraan berlalu lalang. Udara 
+di sini masih segar dan pandangan masih cukup baik. Kita 
+masih bisa melihat matahari terbit dari Gunung Lawu serta 
+matahari tenggelam dengan jelas. Tempat tinggal saya juga 
+dekat dengan pasar dan toserba, ditambah biaya hidup di 
+sini yang masih terjangkau. Sehingga tidak ada kesulitan 
+yang berarti dari saya untuk nyaman berada di sini. 
+    Harapan saya sebagai lulusan Teknik Industri kelak, saya 
+bisa menerapkan ilmu-ilmu yang saya pelajari di Teknik 
+Industri UNS di dalam maupun di luar dunia profesional. Saya 
+juga berharap keaktifan saya dalam menuntut ilmu tidak 
+berhenti dan terus melaksanakan kegiatan ilmiah walau sudah 
+bekerja. 
+""")
--- latihansy.py
+++
@@ -1,3 +0,0 @@
-print("|"+"Hello world!".center(20)+"|")
-print("|"+"Mana info?".center(20)+"|")
-print("|"+"Butuh info".center(20)+"|")
