e986da3efa4ce540867420b9c4630da88f888d12
anggitodwip <gitoprihantoro@gmail.com>
2023-03-12 19:32:05 +0700

    A

--- Anggito.py
+++ Anggito.py
@@ -1,5 +1,5 @@
-print("="+"= ="*10+"=")
+print("="+"= = ="*6+"=")
 print("="+"I N I  P U N Y A".center(30)+"=")
 print("="+"A N G G I T O".center(30)+"=")
 print("="+"S A L A M  K E N A L".center(30)+"=")
-print("="+"= ="*10+"=")
+print("="+"= = ="*6+"=")
